38d7f6677a9893aa0637fd7e078fca3129f1d495
Volcy Salony <salonyvolcy346@gmail.com>
2026-08-14 11:12:31 -0300

    feat: register vertical and horizontal quiz compositions

--- src/remotion/Root.tsx
+++ src/remotion/Root.tsx
@@ -1,24 +1,165 @@
 import React from "react";
-import { Composition } from "remotion";
-import { QuizVertical } from "./compositions/QuizVertical";
 
-export const RemotionRoot: React.FC = () => {
-  const fps = 30;
-  const secondsPerQuestion = 9;
-  const numberOfQuestions = 3;
+import {
+  type CalculateMetadataFunction,
+  Composition,
+} from "remotion";
 
-  return (
-    <>
-      <Composition
-        id="QuizVertical"
-        component={QuizVertical}
-        durationInFrames={
-          fps * secondsPerQuestion * numberOfQuestions
-        }
-        fps={fps}
-        width={1080}
-        height={1920}
-      />
-    </>
-  );
+import {
+  QuizVertical,
+} from "./compositions/QuizVertical";
+
+import {
+  QuizHorizontal,
+} from "./compositions/QuizHorizontal";
+
+import {
+  demoQuiz,
+} from "./data/demoQuiz";
+
+import type {
+  QuizVideoProps,
+} from "./types/quiz";
+
+const calculateDuration =
+  (
+    props: QuizVideoProps
+  ) => {
+    const secondsPerQuestion =
+      props.introSeconds +
+      props.countdownSeconds +
+      props.revealSeconds;
+
+    return Math.max(
+      1,
+
+      Math.ceil(
+        props.questions.length *
+          secondsPerQuestion *
+          props.fps
+      )
+    );
+  };
+
+const calculateVerticalMetadata: CalculateMetadataFunction<
+  QuizVideoProps
+> = ({
+  props,
+}) => {
+  return {
+    durationInFrames:
+      calculateDuration(
+        props
+      ),
+
+    fps:
+      props.fps,
+
+    width:
+      1080,
+
+    height:
+      1920,
+  };
 };
+
+const calculateHorizontalMetadata: CalculateMetadataFunction<
+  QuizVideoProps
+> = ({
+  props,
+}) => {
+  return {
+    durationInFrames:
+      calculateDuration(
+        props
+      ),
+
+    fps:
+      props.fps,
+
+    width:
+      1920,
+
+    height:
+      1080,
+  };
+};
+
+export const RemotionRoot: React.FC =
+  () => {
+    return (
+      <>
+        {/* VERTICAL */}
+
+        <Composition
+          id="QuizVertical"
+
+          component={
+            QuizVertical
+          }
+
+          width={
+            1080
+          }
+
+          height={
+            1920
+          }
+
+          fps={
+            30
+          }
+
+          durationInFrames={
+            30 *
+            9 *
+            3
+          }
+
+          defaultProps={
+            demoQuiz
+          }
+
+          calculateMetadata={
+            calculateVerticalMetadata
+          }
+        />
+
+        {/* HORIZONTAL */}
+
+        <Composition
+          id="QuizHorizontal"
+
+          component={
+            QuizHorizontal
+          }
+
+          width={
+            1920
+          }
+
+          height={
+            1080
+          }
+
+          fps={
+            30
+          }
+
+          durationInFrames={
+            30 *
+            9 *
+            3
+          }
+
+          defaultProps={
+            demoQuiz
+          }
+
+          calculateMetadata={
+            calculateHorizontalMetadata
+          }
+        />
+      </>
+    );
+  };
